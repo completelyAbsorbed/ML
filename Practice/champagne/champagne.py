b6e2cf116dc549569b7b3ccf361818166a7e7fdc
completelyAbsorbed <djfray@gmail.com>
2017-09-19 13:21:09 -0600

    up to step 5.4
    progressed up to step 5.4

--- Practice/champagne/champagne.py
+++ Practice/champagne/champagne.py
@@ -1,5 +1,11 @@
 # champagne.py
 from pandas import Series
+from pandas import TimeGrouper
+from pandas import DataFrame
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+from matplotlib import pyplot
+
 
 
 # https://machinelearningmastery.com/time-series-forecast-study-python-monthly-sales-french-champagne/
@@ -11,4 +17,44 @@ print('Dataset %d, Validation %d' % (len(dataset), len(validation)))
 dataset.to_csv('dataset.csv')
 validation.to_csv('validation.csv')
 
-# start at step 3
+# prepare data
+X = series.values
+X = X.astype('float32')
+train_size = int(len(X) * 0.50)
+train, test = X[0:train_size], X[train_size:]
+
+# walk-forward validation
+history = [x for x in train]
+predictions = list()
+for i in range(len(test)):
+	# predict 
+	yhat = history[-1]
+	predictions.append(yhat)
+	# observation
+	obs = test[i]
+	history.append(obs)
+	print('>Predicted=%3.f, Expected=%3.f' % (yhat, obs))
+# report performance
+mse = mean_squared_error(test, predictions)
+rmse = sqrt(mse)
+print('RMSE: %.3f' % rmse)
+
+print dataset.describe()
+
+## look at the data in a line plot
+#dataset.plot()
+#pyplot.show()
+## look at seasonal line plots
+groups = dataset['1964':'1970'].groupby(TimeGrouper('A'))
+years = DataFrame()
+pyplot.figure()
+i = 1
+n_groups = len(groups)
+for name, group in groups:
+	pyplot.subplot((n_groups*100) + 10 + i)
+	i += 1
+	pyplot.plot(group)
+pyplot.show()
+
+
+# log what step I'm on here : 5.4
