02d21278161b3c07a7b17c86b06aaf1e17b7f34c
completelyAbsorbed <djfray@gmail.com>
2017-09-20 16:53:03 -0600

    filling out process and chrono sequence tools
    developed package and process scripts significantly. on step 6.1 but
    skipped differencing/stationary stuff, need to include that before
    moving on

--- Practice/champagne/ChronSeqTools.py
+++ Practice/champagne/ChronSeqTools.py
@@ -14,8 +14,103 @@ from statsmodels.tsa.arima_model import ARIMAResults
 import numpy
 import warnings
 
+# create ACF and PACF plots
+def autocorrelation_plots(dataset):
+	pyplot.figure()
+	pyplot.subplot(211)
+	plot_acf(dataset, ax=pyplot.gca())
+	pyplot.subplot(212)
+	plot_pacf(dataset, ax=pyplot.gca())
+	pyplot.show()
 
+# print dataset summary and show line, seasonal line, density, box-and-whisker plots
+def summaries(dataset, group_start, group_end, tg_param):
+	print
+	print "  Summary Statistics :"
+	print
+	print dataset.describe()
+	makespace()
+	# line plot
+	dataset.plot()
+	pyplot.show()
+	# seasonal line plots
+	groups = dataset[group_start:group_end].groupby(TimeGrouper(tg_param))
+	years = DataFrame()
+	pyplot.figure()
+	i = 1
+	n_groups = len(groups)
+	for name, group in groups:
+		pyplot.subplot((n_groups*100) + 10 + i)
+		i += 1
+		pyplot.plot(group)
+	pyplot.show()
+	# density plot
+	pyplot.figure(1)
+	pyplot.subplot(211)
+	dataset.hist()
+	pyplot.subplot(212)
+	dataset.plot(kind='kde')
+	pyplot.show()
+	# box-and-whisker plots 
+	years = DataFrame()
+	for name, group in groups:
+		years[name.year] = group.values
+	years.boxplot()
+	pyplot.show()
 
+# persistence model for performance baseline
+def persistence(train, test, print_flag = True):
+	# walk-forward validation
+	if print_flag:
+		print '  Persistence : '
+		print
+	history = [x for x in train]
+	predictions = list()
+	for i in range(len(test)):
+		# predict
+		yhat = history[-1]
+		predictions.append(yhat)
+		# observation
+		obs = test[i]
+		history.append(obs)
+		if print_flag:
+			print('>Predicted=%.3f, Expected=%3.f' % (yhat, obs))
+	# report performance
+	rmse = RMSE(test, predictions, print_flag = False)
+	if print_flag:
+		print
+		print('Persistence RMSE: %.3f' % rmse)
+	return rmse
+
+# prepare data for train test split
+def train_test_split(dataset, train_size_factor = 0.5):
+	X = dataset.values
+	X = X.astype('float32')
+	train_size = int(len(X) * 0.50)
+	train, test = X[0:train_size], X[train_size:]
+	return train, test
+
+# Root Mean Squared Error function
+def RMSE(test, predictions, print_flag = True):
+	mse = mean_squared_error(test, predictions)
+	rmse = sqrt(mse)
+	if print_flag:
+		print('RMSE: %.3f' % rmse)
+	return rmse
+# make some space to make output easier to read 
+def makespace(lines = 5, space_print = ''):
+	for x in range(0, lines):
+		print space_print
+	
+# load data and return/write training, validation sets
+def load(filename, prefix_split, n_last_periods):
+	series = Series.from_csv(filename, header=0)
+	split_point = len(series) - n_last_periods
+	dataset, validation = series[0:split_point], series[split_point:]
+	print('Dataset %d, Validation %d' % (len(dataset), len(validation)))
+	dataset.to_csv(prefix_split + 'Training.csv')
+	validation.to_csv(prefix_split + 'Validation.csv')
+	return dataset, validation
 
 
 # create a differenced series
@@ -50,8 +145,7 @@ def evaluate_arima_model(X, arima_order):
 		predictions.append(yhat)
 		history.append(test[t])
 	# calculate out of sample error
-	mse = mean_squared_error(test, predictions)
-	rmse = sqrt(mse)
+	rmse = RMSE(test, predictions, print_flag = False)
 	return rmse
  
 # evaluate combinations of p, d and q values for an ARIMA model
--- Practice/champagne/cstProcess.py
+++ Practice/champagne/cstProcess.py
@@ -1,1 +1,70 @@
 # cstProcess.py : present a barebones approach to using ChronSeqTools, from beginning to end
+# some code copied or adapted from https://machinelearningmastery.com/time-series-forecast-study-python-monthly-sales-french-champagne/
+import ChronSeqTools as cst
+from pandas import Series
+from ChronSeqTools import makespace
+
+################################################################################################
+# define a series of flags so the program knows which steps we want to run
+# this helps readability, save on compute cycles, and more
+initial_exploratory_flag = True
+initial_ARIMA_flag = False
+
+# end of flag declarations
+################################################################################################
+# declare filepaths, names, and other variables up front so minimal editing of process required
+filename = 'champagne.csv'
+prefix_split = 'champagne'
+training_filename = prefix_split + 'Training.csv'
+validation_filename = prefix_split + 'Validation.csv'
+n_last_periods = 12
+train_size_factor = 0.5
+group_start = '1964'
+group_end = '1970'
+tg_param = 'A'
+# end of non-flag declarations
+################################################################################################
+
+##### initial exploratory analysis
+if initial_exploratory_flag:
+	makespace()
+	# load data and create champagneTraining.csv and champagneValidation.csv
+	dataset, validation = cst.load(filename=filename, prefix_split=prefix_split, n_last_periods=n_last_periods)
+	makespace()
+	# split the dataset(training master set) into train, test
+	train, test = cst.train_test_split(dataset = dataset, train_size_factor = train_size_factor)
+	# persistence : establish performance baseline
+	rmse_persistence = cst.persistence(train = train, test = test)
+	makespace()
+	# summary statistics & line, seasonal line, density, box-and-whisker plots
+	cst.summaries(dataset = dataset, group_start = group_start, group_end = group_end, tg_param = tg_param)
+	# view ACF and PACF plots 
+	cst.autocorrelation_plots(dataset = dataset) # !!!!!!! need to make stationary
+### notes on initial exploratory analysis :
+# 
+# 
+# 
+# 
+# 
+##### initial ARIMA(p, d, q), manual parameters	
+if initial_ARIMA_flag:
+	if not initial_exploratory_flag:
+		dataset = Series.from_csv(filename, header=0)
+		makespace()
+	print 'hi!'
+		
+		
+		
+################################################################################################
+# editing notes
+# 
+# !!! need to make stationary... we are in step 6.1 but skipped differencing. 
+#		probably want to pass something besides dataset
+#		anyway, pick back up here, and compare ACF/PACF plots to example before changing much else
+# 
+# why are we doing train test split in initial exploratory analysis? should probably move this
+# 
+# need to fill in good notes on initial exploratory analysis
+# 
+# end editing notes 
+################################################################################################
