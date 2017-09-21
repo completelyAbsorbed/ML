741a160b8e40443aa82ebce30753d34923050b55
completelyAbsorbed <djfray@gmail.com>
2017-09-21 15:55:18 -0600

    lots of progress with ARIMA, grid search
    coded more for ARIMA, including grid search. next is reviewing residual
    errors

--- Practice/champagne/ChronSeqTools.py
+++ Practice/champagne/ChronSeqTools.py
@@ -14,6 +14,87 @@ from statsmodels.tsa.arima_model import ARIMAResults
 import numpy
 import warnings
 
+# preform grid search on ARIMA, return best configuration, best score
+def run_grid_search_arima(p_grid, d_grid, q_grid, dataset, train_size_factor, trend, full_print_flag = False):
+	dataset = dataset.astype('float32')
+	best_score, best_cfg = float("inf"), None
+	for p in p_grid:
+		for d in d_grid:
+			for q in q_grid:
+				order = (p,d,q)
+				try:
+					rmse = run_arima(p, d, q, dataset, train_size_factor, trend, print_flag = full_print_flag)
+					if rmse < best_score:
+						best_score, best_cfg = rmse, order
+					print('ARIMA%s RMSE=%.3f' % (order,mse))
+				except:
+					continue
+	print('Best ARIMA%s RMSE=%.3f' % (best_cfg, best_score))	
+	return best_score, best_cfg
+
+# do walk-forward ARIMA, return rmse 
+def run_arima(p, d, q, dataset, train_size_factor = 0.5, trend = 'nc', print_flag = True):
+	# load data
+	series = dataset
+	# prepare data
+	X = series.values
+	X = X.astype('float32')
+	train_size = int(len(X) * train_size_factor)
+	train, test = X[0:train_size], X[train_size:]
+	# walk-forward validation
+	history = [x for x in train]
+	predictions = list()
+	for i in range(len(test)):
+		# difference data
+		months_in_year = 12
+		diff = difference_pd(history, months_in_year)
+		# predict
+		model = ARIMA(diff, order=(p,d,q))
+		model_fit = model.fit(trend=trend, disp=0)
+		yhat = model_fit.forecast()[0]
+		yhat = inverse_difference_pd(history, yhat, months_in_year)
+		predictions.append(yhat)
+		# observation
+		obs = test[i]
+		history.append(obs)
+		if print_flag:
+			print('>Predicted=%.3f, Expected=%3.f' % (yhat, obs))
+	# report performance
+	rmse_arima = RMSE(test, predictions, print_flag)
+	return rmse_arima
+	
+# create a differenced ndarray
+def difference_pd(dataset, interval=1):
+	diff = list()
+	for i in range(interval, len(dataset)):
+		value = dataset[i] - dataset[i - interval]
+		diff.append(value)
+	return diff
+ 
+# invert differenced value...
+def inverse_difference_pd(history, yhat, interval=1):
+	return yhat + history[-interval]	
+
+# compute stationary, show line plot 
+def make_stationary(dataset, months_in_year, filename, show_plot = True):
+	X = dataset.astype('float32')
+	stationary = difference(X, months_in_year)
+	stationary.index = dataset.index[months_in_year:]
+	# check if stationary
+	result = adfuller(stationary)
+	print('ADF Statistic: %f' % result[0])
+	print('p-value: %f' % result[1])
+	print('Critical Values:')
+	for key, value in result[4].items():
+		print('\t%s: %.3f' % (key, value))
+	# save
+	stationary.to_csv(filename)
+	# plot
+	if show_plot:
+		stationary.plot()
+		pyplot.show()
+	return stationary
+
 # create ACF and PACF plots
 def autocorrelation_plots(dataset):
 	pyplot.figure()
@@ -119,7 +200,7 @@ def difference(dataset, interval=1):
 	for i in range(interval, len(dataset)):
 		value = dataset[i] - dataset[i - interval]
 		diff.append(value)
-	return numpy.array(diff)
+	return Series(diff)
  
 # invert differenced value
 def inverse_difference(history, yhat, interval=1):
--- Practice/champagne/cstProcess.py
+++ Practice/champagne/cstProcess.py
@@ -1,5 +1,8 @@
 # cstProcess.py : present a barebones approach to using ChronSeqTools, from beginning to end
 # some code copied or adapted from https://machinelearningmastery.com/time-series-forecast-study-python-monthly-sales-french-champagne/
+# ARIMA "rules" to approach https://people.duke.edu/~rnau/arimrule.htm also in arimrule.txt in this repo
+# How to Tune ARIMA Parameters in Python https://machinelearningmastery.com/tune-arima-parameters-python/
+# ARIMA search on MLmastery https://machinelearningmastery.com/?s=arima&submit=Search
 import ChronSeqTools as cst
 from pandas import Series
 from ChronSeqTools import makespace
@@ -8,8 +11,8 @@ from ChronSeqTools import makespace
 # define a series of flags so the program knows which steps we want to run
 # this helps readability, save on compute cycles, and more
 initial_exploratory_flag = True
-initial_ARIMA_flag = False
-
+initial_ARIMA_flag = True
+grid_ARIMA_flag = True
 # end of flag declarations
 ################################################################################################
 # declare filepaths, names, and other variables up front so minimal editing of process required
@@ -17,11 +20,20 @@ filename = 'champagne.csv'
 prefix_split = 'champagne'
 training_filename = prefix_split + 'Training.csv'
 validation_filename = prefix_split + 'Validation.csv'
+stationary_filename = prefix_split + 'Stationary.csv'
 n_last_periods = 12
+months_in_year = 12
 train_size_factor = 0.5
 group_start = '1964'
 group_end = '1970'
 tg_param = 'A'
+trend = 'nc'
+p_1 = 1
+d_1 = 1
+q_1 = 1
+p_grid = range(0,7)
+d_grid = range(0,3)
+q_grid = range(0,7)
 # end of non-flag declarations
 ################################################################################################
 
@@ -38,33 +50,72 @@ if initial_exploratory_flag:
 	makespace()
 	# summary statistics & line, seasonal line, density, box-and-whisker plots
 	cst.summaries(dataset = dataset, group_start = group_start, group_end = group_end, tg_param = tg_param)
+	# make stationary, show plot 
+	stationary = cst.make_stationary(dataset = dataset, months_in_year = months_in_year, filename = stationary_filename)
+	makespace()
 	# view ACF and PACF plots 
-	cst.autocorrelation_plots(dataset = dataset) # !!!!!!! need to make stationary
+	cst.autocorrelation_plots(dataset = stationary) 
 ### notes on initial exploratory analysis :
 # 
-# 
-# 
-# 
-# 
+# initial line plot shows some clear seasonality, perhaps with increasing trend 
+#		(barring most recent year included in the plot)
+# 		there seem to be end-of-year spikes, middle-year humps, and steep decreases before 
+#		end-of-year spikes.
+#		it could be good to model the seasonal component and remove it,
+#		or use differencing with one or two levels to make the series stationary.
+# seasonal line plots show similar shapes
+#		there is a drastic decrease each august, followed by a steep increase through the end
+#		of the year.
+#		The dips and subsequent increases vary somewhat from year to year.
+#		most years have a less drastic decrease in July
+#		the periods leading up to decrease vary somewhat, though appear mostly level 
+# density plot shows that the distribution is not Gaussian (would look more like a normal curve)
+#		additionally, the distribution has a long right tail, perhaps suggesting an exponential distribution
+#		the median values for each year may indicate an increasing trend, though falling in 1970
+#		the spread (body, middle 50%) appears reasonably stable
+#		every year has outliers on the upper portion, with 1968 also having a lower outlier
+# stationary line plot doesn't show obvious trend or seasonality,
+#		therefore the stationary (seasonally differenced) dataset may be a good starting point
+#		for our modeling.
+#		no further distancing may be required, therefore parameter d may be set to 0
+#		...
+#	next step is to find our initial p and q for ARIMA, as our initial d will be 0
+#	p is for Autoregression(AR)
+#	q is for Moving Average(MA)
+# 	...
+# ACF & PACF plots 
+#		Both plots seem to show significant lag at 1 and 12 months, with PACF also showing
+#		notable lag at 13 months.
+#		a good starting point for p and q are 1 (see literature for tuning ARIMA)
+#
 ##### initial ARIMA(p, d, q), manual parameters	
 if initial_ARIMA_flag:
 	if not initial_exploratory_flag:
 		dataset = Series.from_csv(filename, header=0)
 		makespace()
-	print 'hi!'
-		
-		
+	# do ARIMA process (using 1,1,1 parameters to start after some experimentation)
+	rmse_arima_initial = cst.run_arima(p = p_1, d = d_1, q = q_1, dataset = dataset, train_size_factor = train_size_factor, trend = trend, print_flag = True)
+	### notes on initial ARIMA
+	#
+	# using (p,d,q) = (1,1,1) achieved RMSE = 956.958, a significant improvement over persistence!
+	#
+##### grid search ARIMA 		
+if grid_ARIMA_flag:
+	# search for and print best configuration and score among p,d,q grid specifications
+	arima_params_best, rmse_arima_best = cst.run_grid_search_arima(p_grid = p_grid, d_grid = d_grid, q_grid = q_grid, dataset = dataset, train_size_factor = train_size_factor, trend = trend, full_print_flag = False)
+### notes on grid search ARIMA
+#
+# best (p,d,q) = (0,0,1), best RMSE = 939.464, may or may not be a statistically significant improvement
+#
+#### review residual errors
+
+
+
 		
 ################################################################################################
 # editing notes
 # 
-# !!! need to make stationary... we are in step 6.1 but skipped differencing. 
-#		probably want to pass something besides dataset
-#		anyway, pick back up here, and compare ACF/PACF plots to example before changing much else
-# 
 # why are we doing train test split in initial exploratory analysis? should probably move this
-# 
-# need to fill in good notes on initial exploratory analysis
 # 
 # end editing notes 
 ################################################################################################
